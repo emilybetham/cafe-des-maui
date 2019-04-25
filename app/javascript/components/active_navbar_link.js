6b49c7ccf72b38db269a126d681524b5e9df261b
Emily Betham <emily.betham@bath.edu>
2019-04-25 17:38:30 +0200

    my links/tabs in navbar JS now work and take on new css when link is the active url

--- app/javascript/components/active_navbar_link.js
+++ app/javascript/components/active_navbar_link.js
@@ -1,43 +1,15 @@
 const activeNavbarLink = () => {
-  const links = document.querySelectorAll('.navbar-link')
-  // console.log(links);
-  links.forEach((link) => {
-    link.addEventListener('click', (event) => {
+  // create an array of my links so that I don't have to repeat code for each
+  const linksArray = ['home', 'menu', 'our_story', 'live_music', 'contact']
+  linksArray.forEach((link) => {
+    const url = document.URL
+    if (url.endsWith(link)) {
       let activeTab = document.querySelector('.navbar-link.active-tab');
       activeTab.classList.remove('active-tab');
-      activeTab = event.currentTarget
-      activeTab.classList.add('active-tab');
-      document.querySelector('.navbar-link.active-tab').classList.remove('active-tab');
-      const idOfLink = document.getElementById(activeTab.dataset.contentName)
-      console.log(idOfLink);
-      document.getElementById(activeTab.dataset.contentName).classList.add('active-tab')
-      event.currentTarget.classList.add('active-tab')
-    });
+      document.getElementById(link).classList.add('active-tab');
+    }
   });
 }
 
 export { activeNavbarLink };
 
-
-// EXAMPLE FROM RENT YOUR SKATE BELOW - copied same syntax
-
-
-// const tabDynamic = () => {
-
-//   document.querySelectorAll("a.tab-underlined").forEach((tab) => {
-
-//     tab.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       let activeTab = document.querySelector("a.tab-underlined.active");
-//       activeTab.classList.remove("active");
-//       activeTab = event.currentTarget
-//       activeTab.classList.add("active");
-//       document.querySelector(".tab-content.active").classList.remove("active");
-//       document.getElementById(activeTab.dataset.contentName).classList.add('active')
-
-//     });
-
-//   });
-// }
-
-// export { tabDynamic };
