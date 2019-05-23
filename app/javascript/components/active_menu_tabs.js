131af088f0b946f378b93b8821f8a6e57db44999
Emily Betham <emily.betham@bath.edu>
2019-05-23 08:56:40 +0200

    added js on active tabs, still havent implemented scroll bar, working on getting different html to show depending on which tab were on

---
+++ app/javascript/components/active_menu_tabs.js
@@ -0,0 +1,19 @@
+const activeMenuTabs = () => {
+
+  document.querySelectorAll("a.tab-underlined").forEach((tab) => {
+
+    tab.addEventListener("click", (event) => {
+      event.preventDefault();
+      let activeTab = document.querySelector("a.tab-underlined.active");
+      activeTab.classList.remove("active");
+      activeTab = event.currentTarget
+      activeTab.classList.add("active");
+      document.querySelector(".tab-content.active").classList.remove("active");
+      document.getElementById(activeTab.dataset.contentName).classList.add('active')
+
+    });
+
+  });
+}
+
+export { activeMenuTabs };
