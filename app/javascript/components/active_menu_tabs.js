4155842f0b4ed385c20226fce060702b27320c61
Emily Betham <emily.betham@bath.edu>
2019-05-27 11:45:41 +0200

    working on menu page.. did not need to create models after all, all just CSS for the scroll bar etc

--- app/javascript/components/active_menu_tabs.js
+++ app/javascript/components/active_menu_tabs.js
@@ -12,7 +12,6 @@ const activeMenuTabs = () => {
       document.getElementById(activeTab.dataset.contentName).classList.add('active')
 
     });
-
   });
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,10 @@ import "bootstrap";
 // Internal links
 import { activeNavbarLink } from '../components/active_navbar_link';
 import { activeMenuTabs } from '../components/active_menu_tabs';
-
+import { hiddenClassMenu } from '../components/hidden_class_menu';
 
 
 // Now call your functions :
 activeNavbarLink();
 activeMenuTabs();
+hiddenClassMenu();
